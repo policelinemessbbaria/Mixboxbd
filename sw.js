f21d0a8a2c21cea690e61a6f382e16ce1046b3c4
policelinemessbbaria <bpsujon0842@gmail.com>
2026-07-12 10:01:45 +0600

    Update cache name from v5 to v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // প্রতিবার আপডেটে ভার্সন বাড়াবেন (v4 থেকে v5 করে দিন)
-const CACHE_NAME = 'SafaBoxbd-v5'; 
+const CACHE_NAME = 'SafaBoxbd-v6'; 
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); 
